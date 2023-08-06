c86e4e00f06fdecb610773958026f13692aecd5c
nyck33 <nobutaka@gatech.edu>
2023-08-07 00:08:13 +0900

    added connection to postgres docker

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from pydantic import  BaseModel
 from typing import Optional
 from fastapi import HTTPException
 
+from database import init_db
 
-app = FastAPI()
 
+app = FastAPI(title="eslapp_backend", description="Backend for ESL App", version="1.0.0")
+
+init_db(app)
 
 
 
---
+++ settings.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    DB_NAME: str
+    DB_HOST: str
+    DB_PORT: str
+    DB_USER: str
+    DB_PASS: str
+
+
+settings = Settings(_env_file=Path(__file__).parent/'.env', _env_file_encoding='utf-8')
+
